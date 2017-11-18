c1ef803b2b757a3c1a4514370ad6241de920ab98
BeSmart Developer <besmart@besmartbeopen.it>
2017-11-18 17:26:36 +0100

    Corretta formattazione.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,9 +1,9 @@
 /*
  * file: app.js
- * description: it can contains application specific javascript code  
+ * description: it can contains application specific javascript code
 */
 
 // local common.js - DO NOT REMOVE THIS IMPORT unless you're absolutely sure
 import 'common.js'
 
-//Application specific javascript starts from here
+// Application specific javascript starts from here
